3f37a4a2bd3387012679803c65b380f48ea8dd1d
rahul <abc@gmail.com>
2022-03-08 11:12:27 +0530

    first commit from laptop

---
+++ gitproject/src/test/java/gitsample/ManualTest.java
@@ -0,0 +1,19 @@
+package gitsample;
+
+import org.testng.annotations.Test;
+
+public class ManualTest {
+	@Test
+	public void unit() {
+		System.out.println("this is unit testing");
+	}
+	@Test
+	public void integression() {
+		System.out.println("this is integression testing");
+	}
+	@Test
+	public void functional() {
+		System.out.println("this is functional testing");
+	}
+
+}
